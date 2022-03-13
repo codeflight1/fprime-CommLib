8ccd220a680002c1bc54753fa8dfca8653307fcb
VA7NFH <va7nfh@gmail.com>
2022-03-13 14:26:01 -0700

    add encode error testing

---
+++ SpaceWire/test/ut/EncodeStatusTest.cpp
@@ -0,0 +1,76 @@
+TEST(EncodeStatusTest, ADDR_TYPE_MISMATCHTest) {
+  U8 dat[1] = {0xFF};
+
+  SpaceWire::RMAPPacket packet = SpaceWire::RMAPPacket();
+  packet.set(SpaceWire::SpaceWireAddr(0x54, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::SpaceWireAddr(0x76, dat, 0, SpaceWire::SpaceWireAddrType::PHYSICAL, 0),
+             SpaceWire::RMAPPacketType::WriteReply, false, false, true, SpaceWire::RMAPStatus::Success, (U8) 0x57, (U16) 0x05, (U8) 0x00, (U32) 0x2000, (U32) 0, Fw::Buffer(nullptr, 0, 0));
+
+  Fw::Buffer outbuf = Fw::Buffer((U8*) malloc(packet.getLength()), packet.getLength(), 0x02);
+
+  SpaceWire::RMAPEncodeStatus status = packet.encode(outbuf);
+
+  ASSERT_EQ(status, SpaceWire::RMAPEncodeStatus::ADDR_TYPE_MISMATCH);
+}
+
+TEST(EncodeStatusTest, DATA_LEN_OVERRUNTest) {
+  U8 dat[1] = {0xFF};
+
+  SpaceWire::RMAPPacket packet = SpaceWire::RMAPPacket();
+  packet.set(SpaceWire::SpaceWireAddr(0x54, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::SpaceWireAddr(0x76, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::RMAPPacketType::WriteReply, false, false, true, SpaceWire::RMAPStatus::Success, (U8) 0x57, (U16) 0x05, (U8) 0x00, (U32) 0x2000, (U32) 0xFFFFFFFF, Fw::Buffer(nullptr, 0, 0));
+
+  Fw::Buffer outbuf = Fw::Buffer((U8*) malloc(packet.getLength()), packet.getLength(), 0x02);
+
+  SpaceWire::RMAPEncodeStatus status = packet.encode(outbuf);
+
+  ASSERT_EQ(status, SpaceWire::RMAPEncodeStatus::DATA_LEN_OVERRUN);
+}
+
+TEST(EncodeStatusTest, DATA_LEN_MISMATCHTest) {
+  U8 dat[1] = {0xFF};
+
+  SpaceWire::RMAPPacket packet = SpaceWire::RMAPPacket();
+  packet.set(SpaceWire::SpaceWireAddr(0x54, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::SpaceWireAddr(0x76, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::RMAPPacketType::WriteCommand, false, false, true, SpaceWire::RMAPStatus::Success, (U8) 0x57, (U16) 0x05, (U8) 0x00, (U32) 0x2000, (U32) 0xFF, Fw::Buffer(nullptr, 0, 0));
+
+  Fw::Buffer outbuf = Fw::Buffer((U8*) malloc(packet.getLength()), packet.getLength(), 0x02);
+
+  SpaceWire::RMAPEncodeStatus status = packet.encode(outbuf);
+
+  ASSERT_EQ(status, SpaceWire::RMAPEncodeStatus::DATA_LEN_MISMATCH);
+}
+
+TEST(EncodeStatusTest, BUFFER_LEN_MISMATCHTest) {
+  U8 dat[1] = {0xFF};
+
+  SpaceWire::RMAPPacket packet = SpaceWire::RMAPPacket();
+  packet.set(SpaceWire::SpaceWireAddr(0x54, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::SpaceWireAddr(0x76, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::RMAPPacketType::WriteCommand, false, false, true, SpaceWire::RMAPStatus::Success, (U8) 0x57, (U16) 0x05, (U8) 0x00, (U32) 0x2000, (U32) 0, Fw::Buffer(nullptr, 0, 0));
+
+  Fw::Buffer outbuf = Fw::Buffer((U8*) malloc(packet.getLength()), 10, 0x02);
+
+  SpaceWire::RMAPEncodeStatus status = packet.encode(outbuf);
+
+  ASSERT_EQ(status, SpaceWire::RMAPEncodeStatus::BUFFER_LEN_MISMATCH);
+}
+
+/* // Not yet implemented
+TEST(EncodeStatusTest, RMW_DATA_MISMATCHTest) {
+  U8 dat[1] = {0xFF};
+
+  SpaceWire::RMAPPacket packet = SpaceWire::RMAPPacket();
+  packet.set(SpaceWire::SpaceWireAddr(0x54, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::SpaceWireAddr(0x76, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0),
+             SpaceWire::RMAPPacketType::WriteCommand, false, false, true, SpaceWire::RMAPStatus::Success, (U8) 0x57, (U16) 0x05, (U8) 0x00, (U32) 0x2000, (U32) 0xFF, Fw::Buffer(nullptr, 0, 0));
+
+  Fw::Buffer outbuf = Fw::Buffer((U8*) malloc(packet.getLength()), packet.getLength(), 0x02);
+
+  SpaceWire::RMAPEncodeStatus status = packet.encode(outbuf);
+
+  ASSERT_EQ(status, SpaceWire::RMAPEncodeStatus::RMW_DATA_MISMATCH);
+}
+*/
--- SpaceWire/test/ut/Test.cpp
+++ SpaceWire/test/ut/Test.cpp
@@ -6,27 +6,7 @@
 
 #include <cstdlib>
 
-TEST(RMAPPacketTest, AddrTest) {
-  U8 dat[1] = {0xFF};
-
-  SpaceWire::SpaceWireAddr addr = SpaceWire::SpaceWireAddr(0x54, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0);
-  SpaceWire::SpaceWireAddr addr2 = SpaceWire::SpaceWireAddr(0x76, dat, 0, SpaceWire::SpaceWireAddrType::LOGICAL, 0);
-
-  U8 data[0x10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
-
-  Fw::Buffer databuf = Fw::Buffer(data, 0x10, 0x01);
-
-  SpaceWire::RMAPPacket packet = SpaceWire::RMAPPacket();
-  packet.set(addr, addr2, SpaceWire::RMAPPacketType::WriteReply, false, false, true, SpaceWire::RMAPStatus::Success, (U8) 0x57, (U16) 0x05, (U8) 0x00, (U32) 0x2000, (U32) databuf.getSize(), databuf);
-
-  U8* out = (U8*) malloc(packet.getLength());
-
-  Fw::Buffer outbuf = Fw::Buffer(out, packet.getLength(), 0x02);
-
-  SpaceWire::RMAPEncodeStatus status = packet.encode(outbuf);
-
-  std::cout << status << "  " << outbuf << '\n';
-}
+#include "EncodeStatusTest.cpp"
 
 int main(int argc, char* argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
